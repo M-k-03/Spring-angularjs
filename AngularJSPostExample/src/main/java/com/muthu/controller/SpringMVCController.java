6507c57cc2dffb1b1739b05fc639372404f33fdb
M-k-03 <muthukum4r@gmail.com>
2017-04-22 20:14:45 +0530

    Add files via upload
    Initial Commit..
    Created by Muthu...22-04-2017

---
+++ AngularJSPostExample/src/main/java/com/muthu/controller/SpringMVCController.java
@@ -0,0 +1,42 @@
+package com.muthu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class SpringMVCController {
+	
+	@RequestMapping(value = "/PostFormData", method = RequestMethod.POST)
+	public @ResponseBody Person PostService(@RequestBody Person person) {
+		
+		return person;
+	}
+	
+	@RequestMapping(value = "/PostFormDataByParam1", method = RequestMethod.POST)
+	public @ResponseBody Person PostFormDataByParam1(HttpServletRequest request) {
+		
+		Person person = new Person();
+		person.setName(request.getParameter("name"));
+		person.setName(request.getParameter("location"));
+		person.setName(request.getParameter("phone"));
+		return person;
+	}
+
+	@RequestMapping(value = "/PostFormDataByParam", method = RequestMethod.POST)
+	public @ResponseBody Person PostFormDataByParam(@RequestBody Person person) {
+		
+		/*Person person = new Person();
+		person.setName(request.getParameter("name"));
+		person.setName(request.getParameter("location"));
+		person.setName(request.getParameter("phone"));*/
+		return person;
+	}
+}
